c1ba1fa5cabd158688425e5781b7064a6cf49782
Erik Gillespie <erik.gillespie@gmail.com>
2022-04-13 22:35:55 -0400

    Move main, page header, and page footer to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>
+  <main class="max-w-screen-md p-4 mx-auto">
+    <header class="p-4 text-white bg-blue-800 shadow mb-4 text-center">
+      <h1 class="text-4xl">
+        Browsy Paths
+      </h1>
+      <small class="inline-block text-sm italic leading-tight">
+        an interactive tutorial about making links on the Internet
+      </small>
+    </header>
+
     <nuxt />
-  </div>
+
+    <footer class="static bottom-0 text-center leading-tight">
+      <small class="block mb-1">
+        &copy; Copyright 2020-Present,
+        <a
+          class="text-blue-900 hover:underline"
+          href="https://twitter.com/eriklgillespie"
+          target="_blank"
+        >Erik Gillespie</a>.
+      </small>
+      <small class="block mb-1">
+        Feedback or questions?
+        <a
+          class="text-blue-900 hover:underline"
+          href="https://github.com/egillespie/browsy-paths"
+          target="_blank"
+        >Create an issue on GitHub.</a>
+      </small>
+    </footer>
+  </main>
 </template>
 
 <style>
@@ -24,32 +52,7 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+body {
+  @apply bg-blue-100;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,119 +1,91 @@
 <template>
-  <main
-    class="max-w-screen-md p-4 mx-auto"
+  <article
+    class="mb-8"
     @keypress.enter.prevent="onEnterKey"
   >
+    <header class="mb-4 text-center">
+      <h2 class="text-2xl text-gray-900 font-bold">
+        {{ scenario.name }}
+      </h2>
+      <small class="text-lg italic leading-tight">
+        Page {{ scenarioNumber }} of {{ scenarioTotal }}
+      </small>
+    </header>
+    <section v-if="scenario.description" class="mb-4">
+      <h3 class="text-xl text-gray-800 font-bold border-blue-300 border-solid border-b mb-2">
+        Explanation
+      </h3>
+      <render-md :md="stringify(scenario.description)" format="block" />
+    </section>
+    <section class="mb-4">
+      <h3 class="text-xl text-gray-800 font-bold border-blue-300 border-solid border-b mb-2">
+        Exercise
+      </h3>
+      <h4 class="font-semibold mt-4">
+        Website URL
+      </h4>
+      <code class="block bg-white border border-solid border-blue-200 p-2 shadow-sm">
+        {{ scenario.siteUrl }}
+      </code>
+      <h4 class="font-semibold mt-4">
+        Website files
+      </h4>
+      <file-tree
+        :files="scenario.project"
+        class="bg-white border border-solid border-blue-200 p-2 shadow-sm"
+      />
+      <h4 class="font-semibold mt-4">
+        Question
+      </h4>
+      <form class="bg-white border border-solid border-blue-200 p-2 shadow-sm">
+        <label for="answer" class="inline-block mb-2">
+          <render-md :md="scenario.question" />
+        </label>
+        <input
+          id="answer"
+          ref="answer"
+          v-model="answer"
+          type="text"
+          class="inline-block bg-gray-100 w-full px-2 py-1 border border-solid border-gray-400 outline-none focus:border-blue-500 focus:bg-white"
+          placeholder="Type your answer here"
+        >
+      </form>
+    </section>
+    <nav>
+      <ol class="flex justify-around">
+        <li>
+          <game-button
+            :disabled="isFirstScenario"
+            @click="previousScenario"
+          >
+            Prev
+          </game-button>
+        </li>
+        <li>
+          <game-button
+            class="underline"
+            @click="showCheckAnswer"
+          >
+            Check Answer
+          </game-button>
+        </li>
+        <li>
+          <game-button
+            :disabled="isLastScenario"
+            @click="nextScenario"
+          >
+            Next
+          </game-button>
+        </li>
+      </ol>
+    </nav>
     <check-answer-modal
       :show="showCheckAnswerModal"
       :correct="answer === scenario.answer"
       @next="nextScenarioIfCorrectAnswer"
       @close="closeCheckAnswer"
     />
-    <header class="p-4 text-white bg-blue-800 shadow mb-4 text-center">
-      <h1 class="text-4xl">
-        Browsy Paths
-      </h1>
-      <small class="inline-block text-sm italic leading-tight">
-        an interactive tutorial about making links on the Internet
-      </small>
-    </header>
-    <article class="mb-8">
-      <header class="mb-4 text-center">
-        <h2 class="text-2xl text-gray-900 font-bold">
-          {{ scenario.name }}
-        </h2>
-        <small class="text-lg italic leading-tight">
-          Page {{ scenarioNumber }} of {{ scenarioTotal }}
-        </small>
-      </header>
-      <section v-if="scenario.description" class="mb-4">
-        <h3 class="text-xl text-gray-800 font-bold border-blue-300 border-solid border-b mb-2">
-          Explanation
-        </h3>
-        <render-md :md="stringify(scenario.description)" format="block" />
-      </section>
-      <section class="mb-4">
-        <h3 class="text-xl text-gray-800 font-bold border-blue-300 border-solid border-b mb-2">
-          Exercise
-        </h3>
-        <h4 class="font-semibold mt-4">
-          Website URL
-        </h4>
-        <code class="block bg-white border border-solid border-blue-200 p-2 shadow-sm">
-          {{ scenario.siteUrl }}
-        </code>
-        <h4 class="font-semibold mt-4">
-          Website files
-        </h4>
-        <file-tree
-          :files="scenario.project"
-          class="bg-white border border-solid border-blue-200 p-2 shadow-sm"
-        />
-        <h4 class="font-semibold mt-4">
-          Question
-        </h4>
-        <form class="bg-white border border-solid border-blue-200 p-2 shadow-sm">
-          <label for="answer" class="inline-block mb-2">
-            <render-md :md="scenario.question" />
-          </label>
-          <input
-            id="answer"
-            ref="answer"
-            v-model="answer"
-            type="text"
-            class="inline-block bg-gray-100 w-full px-2 py-1 border border-solid border-gray-400 outline-none focus:border-blue-500 focus:bg-white"
-            placeholder="Type your answer here"
-          >
-        </form>
-      </section>
-      <nav>
-        <ol class="flex justify-around">
-          <li>
-            <game-button
-              :disabled="isFirstScenario"
-              @click="previousScenario"
-            >
-              Prev
-            </game-button>
-          </li>
-          <li>
-            <game-button
-              class="underline"
-              @click="showCheckAnswer"
-            >
-              Check Answer
-            </game-button>
-          </li>
-          <li>
-            <game-button
-              :disabled="isLastScenario"
-              @click="nextScenario"
-            >
-              Next
-            </game-button>
-          </li>
-        </ol>
-      </nav>
-    </article>
-    <footer class="static bottom-0 text-center leading-tight">
-      <small class="block mb-1">
-        &copy; Copyright 2020-Present,
-        <a
-          class="text-blue-900 hover:underline"
-          href="https://twitter.com/eriklgillespie"
-          target="_blank"
-        >Erik Gillespie</a>.
-      </small>
-      <small class="block mb-1">
-        Feedback or questions?
-        <a
-          class="text-blue-900 hover:underline"
-          href="https://github.com/egillespie/browsy-paths"
-          target="_blank"
-        >Create an issue on GitHub.</a>
-      </small>
-    </footer>
-  </main>
+  </article>
 </template>
 
 <script>
@@ -196,9 +168,3 @@ export default {
   }
 }
 </script>
-
-<style>
-body {
-  @apply bg-blue-100;
-}
-</style>
