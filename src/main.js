b804cc817abdfbcb8e92431bf840f429a7f268cf
Erik Gillespie <erik.gillespie@gmail.com>
2022-04-24 22:11:44 -0400

    Add store to show page number based on route

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+router.beforeEach((to, _from, next) => {
+  const scenarios = store.getters['pages/all']
+  const scenarioIndex = scenarios.indexOf(to.path)
+  if (scenarioIndex >= 0) {
+    store.commit('pages/set', scenarioIndex)
+    next()
+  } else {
+    next('/')
+  }
+})
+
 createApp(App).use(store).use(router).mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,67 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    pages: {
+      namespaced: true,
+
+      state: {
+        currentIndex: 0,
+        scenarios: [
+          '/',
+          '/about'
+        ]
+      },
+      getters: {
+        all (state) {
+          return state.scenarios
+        },
+        index (state) {
+          return state.currentIndex
+        },
+        number (state) {
+          return state.currentIndex + 1
+        },
+        current (state) {
+          return state.scenarios[state.currentIndex]
+        },
+        isFirst (state) {
+          return state.currentIndex === 0
+        },
+        isLast (state) {
+          return state.currentIndex === state.scenarios.length - 1
+        },
+        total (state) {
+          return state.scenarios.length
+        }
+      },
+
+      mutations: {
+        set (state, newIndex) {
+          if (newIndex < 0) {
+            state.currentIndex = 0
+          } else if (newIndex >= state.scenarios.length) {
+            state.currentIndex = state.scenarios.length - 1
+          } else {
+            state.currentIndex = newIndex
+          }
+        }
+      },
+
+      actions: {
+        next ({ state, commit }, { router }) {
+          if (state.currentIndex < state.scenarios.length - 1) {
+            commit('set', state.currentIndex + 1)
+            router.push(state.scenarios[state.currentIndex])
+          }
+        },
+        previous ({ state, commit }, { router }) {
+          if (state.currentIndex > 0) {
+            commit('set', state.currentIndex - 1)
+            router.push(state.scenarios[state.currentIndex])
+          }
+        }
+      }
+    }
   }
 })
